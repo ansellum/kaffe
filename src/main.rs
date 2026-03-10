130e57b65a51d7066a4f31f157e9ad2e0cf31282
ansellum <marche@Shibuya.(none)>
2026-03-09 19:33:13 -0700

    Added TODO

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,13 @@ pub mod bean;
 
 fn main() {
     println!("Hello, world!");
+
+    // TODO:
+    // - Read Ch 9 -> Ch 12 of The Rust Programming Language book
+    //      9 - Error Handling
+    //     10 - Generic Types (optional)
+    //     11 - Automated tests
+    //     12 - I/O Project
+    //
+    // - Review Ch 8, take notes if necessary
 }
