fb8a3408afa6a2a69c7d554e32a715310f8b054c
ansellum <jacobvogt27@gmail.com>
2026-03-19 18:13:44 -0700

    Moved TODO

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # kaffe
 A self-hosted coffee tracking app built using Rust
+
+# TODO
+- Define command line input
+- Write helper default function for wrong command input
+- Call respective functions per command
--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,4 @@ pub mod bean;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-
-    // TODO:
-    // - Read Ch 9 -> Ch 12 of The Rust Programming Language book
-    //      9 - Error Handling
-    //     10 - Generic Types (optional)
-    //     11 - Automated tests
-    //     12 - I/O Project
-    //
-    // - Review Ch 8, take notes if necessary
 }
