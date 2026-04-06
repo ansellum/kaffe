6e9528b9d7b1d9b5635a7ab063f637bc36ef2016
Aesir <jacobvogt27@gmail.com>
2026-04-06 10:52:41 -0700

    covfefe

--- src/coffee.rs
+++ src/coffee.rs
@@ -10,7 +10,10 @@ enum CoffeeKind {
 
 impl fmt::Display for CoffeeKind {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:?}", self)
+        match self {
+            Self::SingleOrigin => write!(f, "single-origin"),
+            Self::Blend => write!(f, "blend"),
+        }
     }
 }
 
@@ -19,8 +22,8 @@ impl std::str::FromStr for CoffeeKind {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
-            "single-origin" => Ok(EquipmentKind::Grinder),
-            "brewer" => Ok(EquipmentKind::Brewer),
+            "single-origin" => Ok(Self::SingleOrigin),
+            "brewer" => Ok(Self::Blend),
             _ => Err(()),
         }
     }
@@ -35,10 +38,26 @@ enum RoastLevel {
 
 impl fmt::Display for RoastLevel {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:?}", self)
+        match self {
+            Self::Dark => write!(f, "dark"),
+            Self::Medium => write!(f, "medium"),
+            Self::Light => write!(f, "light"),
+        }
     }
 }
 
+impl std::str::FromStr for RoastLevel {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "dark" => Ok(Self::Dark),
+            "medium" => Ok(Self::Medium),
+            "light" => Ok(Self::Light),
+            _ => Err(()),
+        }
+    }
+}
 
 #[derive(Debug)]
 pub struct Coffee {
@@ -57,9 +76,9 @@ pub struct Coffee {
     process: Option<String>,
     decaf: bool,
 
-    varietals: Option<String>,
-    region: Option<String>,
-    tasting_notes: String,
+    varietals: Option<Vec<String>>,
+    region: Option<Vec<String>>,
+    tasting_notes: Vec<String>,
 
     timestamp: Timestamp,
 }
@@ -71,12 +90,12 @@ impl Coffee {
                 VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}')",
             self.roaster,
             self.name,
-            self.roast_level.to_string().to_lowercase(),
-            self.kind.to_string().to_lowercase(), 
-            self.country.as_deref().unwrap_or_default(),
-            self.farm.as_deref().unwrap_or_default(),
+            self.roast_level.to_string(),
+            self.kind.to_string(),
+            self.country.unwrap_or_default(),
+            self.farm.unwrap_or_default(),
             self.producer.as_deref().unwrap_or_default(),
-            self.altitude_m.map(|v| v.to_string()).unwrap_or("default".to_string()),
+            self.altitude_m.map(|v| v.to_string()).unwrap_or("".to_string()),
             self.altitude_lower_m.unwrap_or_default(),
             self.altitude_upper_m.unwrap_or_default(),
             self.process.as_deref().unwrap_or_default(),
@@ -94,25 +113,25 @@ pub fn new(record: csv::StringRecord) -> Result<Coffee, Box<dyn Error>> {
         id: 0,
         roaster: record[0].to_string(),
         name: record[1].to_string(),
-        roast_level: RoastLevel,
-        kind: CoffeeKind,
-        country: Option<String>,
-        farm: Option<String>,
-        producer: Option<String>,
-        altitude_m: Option<u16>,
-        altitude_lower_m: Option<u16>,
-        altitude_upper_m: Option<u16>,
-        process: Option<String>,
-        decaf: bool,
-
-        varietals: Option<String>,
-        region: Option<String>,
-        tasting_notes: String,
-
-        timestamp: Timestamp,
+        kind: record[2].parse().expect("CoffeeKind parse error!"),
+        country: none_if_empty(record[3].to_string()),
+        region: none_if_empty(record[4].to_string()).map(|s|
+            s.split(';').map(str::to_owned).collect()),
+        farm: none_if_empty(record[5].to_string()),
+        producer: none_if_empty(record[6].to_string()),
+        varietals: none_if_empty(record[7].to_string()).map(|s|
+            s.split(';').map(str::to_owned).collect()),
+        altitude_m: none_if_empty(record[8].to_string()).map(|s| s.parse::<u16>().expect("altitude_m Parse Error")),
+        altitude_lower_m: none_if_empty(record[9].to_string()).map(|s| s.parse::<u16>().expect("altitude_lower_m Parse Error")),
+        altitude_upper_m: none_if_empty(record[10].to_string()).map(|s| s.parse::<u16>().expect("altitude_upper_m Parse Error")),
+        process: none_if_empty(record[3].to_string()),
+        roast_level: record[11].parse().expect("RoastLevel parse error!"),
+        tasting_notes: record[12].split(';').map(str::to_owned).collect(),
+        decaf: !record[13].is_empty(),
+        timestamp: Timestamp::now(),
     };
 
-    Ok(e)
+    Ok(c)
 }
 
 // pub fn new(mut c: Coffee) -> Result<Coffee, Box<dyn Error>> {
@@ -126,6 +145,18 @@ pub fn new(record: csv::StringRecord) -> Result<Coffee, Box<dyn Error>> {
 //     Ok(c)
 // }
 
-// fn json_array_from_delimited(str: String) -> String {
-//     format!("{:?}", str.split(';').collect::<Vec<_>>())
-// }
+fn json_array_from_delimited(str: String) -> String {
+    format!("{:?}", str.split(';').collect::<Vec<_>>())
+}
+
+fn empty_to_opt_str(field: &str) -> Option<String> {
+    (!field.is_empty()).then(|| field.to_string())
+}
+
+fn empty_to_opt_u16(field: &str) -> Option<u16> {
+    (!field.is_empty()).then(|| field.parse().expect("Not a valid number"))
+}
+
+fn none_if_empty(field: String) -> Option<String> {
+    if field.is_empty() { None } else { Some(field) }
+}
