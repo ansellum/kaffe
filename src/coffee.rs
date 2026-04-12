58a586c8089fe4b13f21522a04ee53b8c174db34
ansellum <marche@Shibuya.(none)>
2026-04-11 23:00:06 -0700

    Finished and tested coffee wizard

--- src/coffee.rs
+++ src/coffee.rs
@@ -1,10 +1,12 @@
 use jiff::Timestamp;
+use rusqlite::Connection;
 use std::fmt;
 use std::error::Error;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
+use inquire::error::CustomUserError;
 
 #[derive(Debug)]
-pub enum CoffeeKind {
+enum CoffeeKind {
     SingleOrigin,
     Blend
 }
@@ -22,7 +24,7 @@ impl std::str::FromStr for CoffeeKind {
     type Err = ();
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
+        match s.to_lowercase().as_str() {
             "single-origin" => Ok(Self::SingleOrigin),
             "blend" => Ok(Self::Blend),
             _ => Err(()),
@@ -51,7 +53,7 @@ impl std::str::FromStr for RoastLevel {
     type Err = ();
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
+        match s.to_lowercase().as_str() {
             "dark" => Ok(Self::Dark),
             "medium" => Ok(Self::Medium),
             "light" => Ok(Self::Light),
@@ -67,16 +69,16 @@ pub struct Coffee {
     roast_level: RoastLevel,
     kind: CoffeeKind,
     country: Option<String>,
+    region: Option<String>,
     farm: Option<String>,
     producer: Option<String>,
+    varietals: Option<Vec<String>>,
     altitude_m: Option<u16>,
     altitude_lower_m: Option<u16>,
     altitude_upper_m: Option<u16>,
     process: Option<String>,
     decaf: bool,
 
-    varietals: Option<Vec<String>>,
-    region: Option<Vec<String>>,
     tasting_notes: Vec<String>,
 
     timestamp: Timestamp,
@@ -85,7 +87,7 @@ pub struct Coffee {
 impl Coffee {
     pub fn to_sql(&self) -> String {
         format!(
-            "INSERT INTO coffee (roaster, name, roast_level, kind, country, farm, producer, altitude_m, altitude_lower_m, altitude_upper_m, process, decaf, varietals, region, tasting_notes, timestamp) 
+            "INSERT INTO coffee (roaster, name, roast_level, kind, country, region, farm, producer, varietals, altitude_m, altitude_lower_m, altitude_upper_m, process, decaf, tasting_notes, timestamp) 
                 VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}')",
             self.roaster,
             self.name,
@@ -94,12 +96,18 @@ impl Coffee {
             self.country
                 .as_deref()
                 .unwrap_or_default(),
+            self.region
+                .as_deref()
+                .unwrap_or_default(),
             self.farm
                 .as_deref()
                 .unwrap_or_default(),
             self.producer
                 .as_deref()
                 .unwrap_or_default(),
+            self.varietals
+                .as_deref()
+                .map_or_else(|| String::new(), |s| format!("{:?}", s)),
             self.altitude_m.map_or(String::new(), |num| num.to_string()),
             self.altitude_lower_m.map_or(String::new(), |num| num.to_string()),
             self.altitude_upper_m.map_or(String::new(), |num| num.to_string()),
@@ -107,12 +115,6 @@ impl Coffee {
                 .as_deref()
                 .unwrap_or_default(),
             self.decaf,
-            self.varietals
-                .as_deref()
-                .map_or_else(|| String::new(), |s| format!("{:?}", s)),
-            self.region
-                .as_deref()
-                .map_or_else(|| String::new(), |s| format!("{:?}", s)),
             format!("{:?}", self.tasting_notes),
             self.timestamp.to_string()
         )
@@ -145,13 +147,11 @@ pub fn new(soul: HashMap<&str, &str>) -> Result<Coffee, Box<dyn Error>> {
     let c = Coffee {
         roaster: soul["roaster"].to_string(),
         name: soul["name"].to_string(),
-        kind: soul["kind"].parse::<CoffeeKind>().expect("CoffeeKind parse error!"),
+        kind: soul["kind"]
+            .parse::<CoffeeKind>()
+            .expect("CoffeeKind parse error!"),
         country: none_if_empty(soul["country"]),
-        region: none_if_empty(soul["region"])
-            .map(|s| s.split(';')
-                .map(str::to_owned)
-                .collect()
-            ),
+        region: none_if_empty(soul["region"]),
         farm: none_if_empty(soul["farm"]),
         producer: none_if_empty(soul["producer"]),
         varietals: none_if_empty(soul["varietals"])
@@ -173,7 +173,8 @@ pub fn new(soul: HashMap<&str, &str>) -> Result<Coffee, Box<dyn Error>> {
             ),
         process: none_if_empty(soul["process"]),
         roast_level: soul["roast_level"]
-            .parse().expect("RoastLevel parse error!"),
+            .parse::<RoastLevel>()
+            .expect("RoastLevel parse error!"),
         tasting_notes: soul["tasting_notes"]
             .split(';')
             .map(str::to_owned)
@@ -187,4 +188,58 @@ pub fn new(soul: HashMap<&str, &str>) -> Result<Coffee, Box<dyn Error>> {
 
 fn none_if_empty(field: &str) -> Option<String> {
     if field.is_empty() { None } else { Some(field.to_string()) }
+}
+
+pub fn roaster_suggestor(input: &str) -> Result<Vec<String>, CustomUserError> {
+    suggestor(input, "SELECT roaster FROM coffee")
+}
+
+pub fn country_suggestor(input: &str) -> Result<Vec<String>, CustomUserError> {
+    suggestor(input, "SELECT country FROM coffee")
+}
+
+pub fn region_suggestor(input: &str) -> Result<Vec<String>, CustomUserError> {
+    suggestor(input, "SELECT region FROM coffee")
+}
+
+pub fn farm_suggestor(input: &str) -> Result<Vec<String>, CustomUserError> {
+    suggestor(input, "SELECT farm FROM coffee")
+}
+
+pub fn producer_suggestor(input: &str) -> Result<Vec<String>, CustomUserError> {
+    suggestor(input, "SELECT producer FROM coffee")
+}
+
+fn suggestor(input: &str, sql: &str) -> Result<Vec<String>, CustomUserError> {
+    let input = input.to_lowercase();
+
+    Ok(get_suggestions_from_db(sql)?
+        .into_iter()
+        .collect::<HashSet<_>>() // Remove duplicates
+        .into_iter()
+        .filter(|p| !p.is_empty())
+        .filter(|p| p.to_lowercase().contains(&input))
+        .take(5)
+        .map(|p| String::from(p))
+        .collect())
+}
+
+fn get_suggestions_from_db(sql: &str) -> Result<Vec<String>, rusqlite::Error>{
+    let conn = Connection::open("./kaffe.db")?;
+
+    // Prepare the query
+    let mut stmt = conn.prepare(sql)?;
+
+    // Query and map rows to strings
+    let suggestions_iter = stmt.query_map([], |row| {
+        row.get::<_, String>(0) // Get the first column as String
+    })?;
+
+    // Collect into a Vec
+    let mut suggestion_list = Vec::new();
+    for suggestion in suggestions_iter {
+        suggestion_list.push(suggestion?);
+    }
+
+    Ok(suggestion_list)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use clap::{Parser, Subcommand};
 use rusqlite::Connection;
 use std::fs;
-use std::fmt;
 use std::error::Error;
 use std::collections::HashMap;
 
@@ -34,8 +33,8 @@ enum Modes {
 enum Items {
     Equipment,
     Coffee,
-    Bag,
-    Brew
+    // Bag,
+    // Brew
 }
 
 impl std::str::FromStr for Items {
@@ -45,8 +44,8 @@ impl std::str::FromStr for Items {
         match s {
             "Equipment" => Ok(Self::Equipment),
             "Coffee" => Ok(Self::Coffee),
-            "Bag" => Ok(Self::Bag),
-            "Brew" => Ok(Self::Brew),
+            // "Bag" => Ok(Self::Bag),
+            // "Brew" => Ok(Self::Brew),
             _ => Err(())
         }
     }
@@ -78,7 +77,7 @@ fn import_from_csv(path: &str) -> Result<(), Box<dyn Error>> {
                 conn.execute(&e.to_sql(), [])?;
             }
             15 => { // COFFEE
-                let c = coffee::new(record, &header_map)?;
+                let c = coffee::new_csv(record, &header_map)?;
                 conn.execute(&c.to_sql(), [])?;
             }
             6 => { // BAGS
@@ -103,25 +102,148 @@ fn equipment_wizard() -> Result<(), Box<dyn Error>> {
                 .with_help_message("Name your vessel.")
                 .prompt()?;
 
-    let _kind = Select::new("Kind:", vec!["Brewer", "Grinder"]).prompt()?;
+    let _kind = Select::new("Kind:", vec!["brewer", "grinder"])
+        .prompt()?
+        .to_string();
 
     let _purchase_date = DateSelect::new("Purchase Date:")
-        .prompt()?;
+        .with_help_message("When was your vessel acquired?")
+        .prompt()?
+        .to_string();
+
+    let _decomission_date = DateSelect::new("Decomission Date:")
+        .with_help_message("When was your vessel disowned?")
+        .prompt_skippable()?
+        .map_or(String::new(), |t| t.to_string());
 
     let _price: f64 = CustomType::new("Amount:")
         .with_formatter(&|i: f64| format!("${i}"))
         .with_error_message("That isn't right.")
-        .with_help_message("How much did this cost you?")
+        .with_help_message("How much did it cost you?")
         .prompt()
         .unwrap();
 
     let price_ct = (_price  * 100.0).trunc().to_string();
 
-    let e = equipment::new(_name, _kind.to_string(), _purchase_date.to_string(), String::new(), price_ct)?;
+    let e = equipment::new(HashMap::from([
+        ("name", _name),
+        ("kind", _kind),
+        ("purchase_date", _purchase_date),
+        ("decomission_date", _decomission_date),
+        ("price_ct", price_ct),
+    ]))?;
 
     //let conn = Connection::open_in_memory()?;
     let conn = Connection::open("./kaffe.db")?;
     conn.execute(&e.to_sql(), [])?;
+
+    println!("Your entry has been successfully recorded.");
+    println!("We thank you for your participation.");
+
+    Ok(())
+}
+fn coffee_wizard() -> Result<(), Box<dyn Error>> {
+    let roaster = Text::new("Roaster:")
+        .with_validator(required!("You can't skip this."))
+        .with_help_message("Name the roaster.")
+        .with_autocomplete(&coffee::roaster_suggestor)
+        .prompt()?;
+
+    let name = Text::new("Name:")
+        .with_validator(required!("You can't skip this."))
+        .with_help_message("Name the coffee.")
+        .with_placeholder("Ethiopia Yirgacheffe")
+        .prompt()?;
+
+    let roast_level = Select::new("Roast Level:", vec!["dark", "medium", "light"])
+        .prompt()?;
+
+    let kind = Select::new("Type:", vec!["single-origin", "blend"])
+        .prompt()?;
+
+    let mut country = String::new();
+    let mut region = String::new();
+    let mut farm = String::new();
+    let mut producer = String::new();
+    let mut varietals = String::new();
+    let mut altitude_m = String::new();
+    let mut altitude_lower_m = String::new();
+    let mut altitude_upper_m = String::new();
+
+    if kind == "single-origin" {
+        country = Text::new("Country:")
+            .with_placeholder("Ethiopia")
+            .with_validator(required!("You chose this."))
+            .with_autocomplete(&coffee::country_suggestor)
+            .prompt()?;
+
+        region = Text::new("Region:")
+            .with_placeholder("Bener Meriah, Aceh")
+            .with_autocomplete(&coffee::region_suggestor)
+            .prompt()?;
+
+        farm = Text::new("Farm:")
+            .with_placeholder("Dawencho")
+            .with_autocomplete(&coffee::farm_suggestor)
+            .prompt()?;
+        
+        producer = Text::new("Producer:")
+            .with_placeholder("Mullugeta Muntasha")
+            .with_autocomplete(&coffee::producer_suggestor)
+            .prompt()?;
+
+        varietals = Text::new("Varietals:")
+            .with_help_message("Enter each varietal separated by semi-colons")
+            .with_placeholder("abyssinia;typica")
+            .prompt()?;
+        altitude_m = Text::new("Altitude (MASL):")
+            .with_help_message("If alitude is given as a range, skip this field")
+            .prompt()?;
+        
+        if altitude_m.is_empty() {
+            altitude_lower_m = Text::new("Altitude Lower (MASL):")
+                .prompt()?;
+            altitude_upper_m = Text::new("Altitude Upper (MASL):")
+                .prompt()?;
+        }
+    }
+
+    let process = Select::new("Process:", vec!["natural", "washed", "wet-hulled", "honey"])
+        .prompt_skippable()?
+        .unwrap_or_default();
+
+    let decaf = match Select::new("Decaf:", vec!["Yes", "No"]).prompt()? {
+        "Yes" => "yippee!".to_string(),
+        _ => String::new(),
+    };
+
+    let tasting_notes = Text::new("Tasting notes:")
+        .with_help_message("Enter each tasting note separated by semi-colons")
+        .with_placeholder("lemon;red fruit;ginger")
+        .with_validator(required!("C'mon, it's on the label"))
+        .prompt()?;        
+
+    let c = coffee::new(HashMap::from([
+        ("roaster", roaster.as_str()),
+        ("name", name.as_str()),
+        ("roast_level", roast_level),
+        ("kind", kind),
+        ("country", country.as_str()),
+        ("region", region.as_str()),
+        ("farm", farm.as_str()),
+        ("producer", producer.as_str()),
+        ("varietals", varietals.as_str()),
+        ("altitude_m", altitude_m.as_str()),
+        ("altitude_lower_m", altitude_lower_m.as_str()),
+        ("altitude_upper_m", altitude_upper_m.as_str()),
+        ("process", process),
+        ("decaf", decaf.as_str()),
+        ("tasting_notes", tasting_notes.as_str()),
+    ]))?;
+
+    //let conn = Connection::open_in_memory()?;
+    let conn = Connection::open("./kaffe.db")?;
+    conn.execute(&c.to_sql(), [])?;
 
     println!("Your entry has been successfully recorded.");
     println!("We thank you for your participation.");
@@ -138,8 +260,8 @@ fn wizard() -> Result<(), Box<dyn Error>> {
     match category {
         Items::Equipment => equipment_wizard(),
         Items::Coffee => coffee_wizard(),
-        Items::Bag=> bag_wizard(),
-        Items::Brew => brew_wizard(),
+        // Items::Bag=> bag_wizard(),
+        // Items::Brew => brew_wizard(),
     }
 }
 
